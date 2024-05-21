bad56e4c7ee67c316c8543c5669ca3828a123ca9
Giovanna Narayana Espinosa Jara <gi.espinosa@duocuc.cl>
2024-05-20 23:20:09 -0400

    html bases para categorias y productos.

--- js/main.js
+++ js/main.js
@@ -13,19 +13,3 @@ document.addEventListener("DOMContentLoaded", function(){
 
 //FUNCIONES : VISUALIZACION DE CARRITO, MODO OSCURO CLARO, OTRAS FUNCIONES VALIDAR ENTRADAS DE DATOS con jquery
 
-
-// declara una var en js
-//resultado de expresiones con diferentes tipos de datos
-// metodos para muestra de informacion 
-// operadores
-// metodos para seleccionar elementos
-// odenar arrays
-//fi/ltras elementos
-// variables
-// tipos de datos
-// JQuery seleccion de elementos
-// funciones de elementos en JQuery (ocultar)
-// funcion anonima
-//agregar evento en JQuery
-// metodos de envio de solicitud con hppts 
-//objeto de aijac con js
